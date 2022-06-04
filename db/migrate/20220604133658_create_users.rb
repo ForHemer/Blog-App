a2f66581ad3b9707aa57d96aede112de76224604
ForHemer <foreste.hemerson@gmail.com>
2022-06-04 09:57:32 -0400

    Update user table

--- db/migrate/20220604133658_create_users.rb
+++ db/migrate/20220604133658_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :photo
-      t.text :bio
+      t.string :Name
+      t.string :Photo
+      t.text :Bio
       t.integer :PostsCounter
       
       t.timestamps
