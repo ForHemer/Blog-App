2fd339d46d2ceb9b75ad1d1a64f3dff24fdf1087
ForHemer <foreste.hemerson@gmail.com>
2022-06-07 21:41:11 -0400

    Create custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  def self.most_recent_posts(user)
+    Post.where(author: user).order(created_at: :desc).limit(3)
+  end
 end
