934f2912ebeeb1632817245f987023092a112693
ForHemer <foreste.hemerson@gmail.com>
2022-06-11 07:59:05 -0400

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
-Rails.application.routes.draw do
-  # get 'users/index'
- # get 'users/show'
- resources :users do
-  resources :posts
- end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+Rails.application.routes.draw do    
   root "users#index"
-  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
