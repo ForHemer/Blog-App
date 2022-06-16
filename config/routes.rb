bb28510f4694236faa7ddc46300cd7315e3c40e4
ForHemer <foreste.hemerson@gmail.com>
2022-06-16 06:27:04 -0400

    Remove commented code

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   resources :posts 
   end
 
-#   # resources :comments, path: "/users/:user_id/posts/:post_id/comment", only: [:new, :create, :destroy]
-#  resources :likes, path: "/users/::user_id/posts/:post_id/like", to: "likes#like"
-
 get '/users/:user_id/posts/:post_id/comment', to: 'comments#new', as: 'new_comment'
 post '/users/:user_id/posts/:post_id/comment', to: 'comments#create' #
 get '/users/:user_id/posts/:post_id/like', to: 'likes#like'
